ff50ae217858835ab01086396e3bae62eb2d44b2
pxai <pello_altadill@cuatrovientos.org>
2017-01-26 19:44:57 +0100

    Dockerfile for java

---
+++ java/src/io/pello/docker/Main.java
@@ -0,0 +1,10 @@
+package io.pello.docker;
+/**
+* Simple java Main class
+*/
+public class Main {
+    public static void main (String args[]) {
+        System.out.println("Docker rules, you suck");
+        System.out.println("Dockerized Java app is working!");
+    }
+}

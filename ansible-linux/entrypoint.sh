2bb75ac99d4101ba0715726c10c39f5d41733632
Pello Altadill <pello.altadill@gluckgames.com>
2019-11-07 00:21:30 +0100

    One more step

--- ansible-linux/entrypoint.sh
+++ ansible-linux/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Generate temporary SSH key to allow access to the host machine.
-mkdir -p ${HOME}/.ssh
-ssh-keygen -f ${HOME}/.ssh/id_rsa -P ""
+HOMEDIR=${HOME}
+KEYNAME=docker-ansible
+
+mkdir -p ${HOMEDIR}/.ssh 
+ssh-keygen -t rsa -f ${HOMEDIR}/.ssh/${KEYNAME} -P ""
 
 cp /hostssh/authorized_keys /hostssh/authorized_keys.bak
-cat ${HOME}/.ssh/id_rsa.pub >>/hostssh/authorized_keys
+cat ${HOMEDIR}/.ssh/${KEYNAME}.pub >> /hostssh/authorized_keys
 
 # ansible all -i "localhost," -m raw -a "apt-get install -y python-minimal"
 # ansible-playbook -i "localhost," "$@"
